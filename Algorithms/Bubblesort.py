dce178e84aa13efb281da2e78e7907655c8d2d2a
Hemanth Sai Krishna <139855530+hemanthxai@users.noreply.github.com>
2024-08-23 12:29:00 +0530

    Update Bubblesort.py

--- Algorithms/Bubblesort.py
+++ Algorithms/Bubblesort.py
@@ -1,1 +1,14 @@
+def bubblesort(a: list):
+    # Loop through each element in the list
+    for i in range(len(a)):
+        # Inner loop to compare adjacent elements
+        for j in range(0, len(a) - i - 1):
+            # If the current element is greater than the next, swap them
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a  # Return the sorted list
 
+
+arr = [9, 3, 1, 5, 7, 4, 9, 6, 2]
+
+print(bubblesort(arr))
